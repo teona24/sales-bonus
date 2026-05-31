4a529d57bb5cbd6c309bb3bf70318e4b20a92418
Teona <teona.chelidze@yandex.ru>
2026-05-31 15:54:21 +0300

    feat: add new functions

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчет выручки от операции
+  // @TODO: Расчет выручки от операции
+  const { discount, sale_price, quantity } = purchase;
+  const decimalDiscount = discount / 100;
+  const totalPrice = sale_price * quantity;
+  const revenue = totalPrice * (1 - decimalDiscount);
+  return revenue;
 }
 
 /**
@@ -16,7 +21,18 @@ function calculateSimpleRevenue(purchase, _product) {
  * @returns {number}
  */
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
+  // @TODO: Расчет бонуса от позиции в рейтинге
+  const { profit } = seller;
+
+  if (index === 0) {
+    return profit * 0.15;
+  } else if (index === 1 || index === 2) {
+    return profit * 0.1;
+  } else if (index === total - 1) {
+    return 0;
+  } else {
+    return profit * 0.05;
+  }
 }
 
 /**
@@ -26,19 +42,52 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+  // @TODO: Проверка входных данных
 
-    // @TODO: Проверка наличия опций
+  if (!data || !options) {
+    throw new Error("Данные или опции не переданы");
+  };
+  const { calculateRevenue, calculateBonus } = options;
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+  // @TODO: Проверка наличия опций
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+  if (!calculateRevenue || !calculateBonus) {
+    throw new Error("Не переданы функции для расчета выручки и бонусов");
+  }
 
-    // @TODO: Расчет выручки и прибыли для каждого продавца
+  if (!data.sellers || data.sellers.length === 0) {
+    throw new Error("Нет данных о продавцах")
+  }
 
-    // @TODO: Сортировка продавцов по прибыли
+  if (!data.products || data.products.length === 0) {
+    throw new Error("Нет данных о продуктах")
+  }
 
-    // @TODO: Назначение премий на основе ранжирования
+  if (!data.purchase_records || data.purchase_records.length === 0) {
+    throw new Error("Нет данных о покупках")
+  }
 
-    // @TODO: Подготовка итоговой коллекции с нужными полями
+  // @TODO: Подготовка промежуточных данных для сбора статистики
+
+  const sellerStats = {};
+  data.sellers.forEach(seller => {
+    sellerStats[seller.id] = {
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {}
+    };
+  });
+
+  // @TODO: Индексация продавцов и товаров для быстрого доступа
+
+  // @TODO: Расчет выручки и прибыли для каждого продавца
+
+  // @TODO: Сортировка продавцов по прибыли
+
+  // @TODO: Назначение премий на основе ранжирования
+
+  // @TODO: Подготовка итоговой коллекции с нужными полями
 }
